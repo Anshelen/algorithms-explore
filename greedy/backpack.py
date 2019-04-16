32a7dd857b0a77a6b911142be446d4a899987479
Anton Shelenkov <anshelen@yandex.ru>
2019-04-16 16:39:20 +0300

    feat: Add algorithm using binary heap tree for backpack problem

--- greedy/backpack.py
+++ greedy/backpack.py
@@ -5,8 +5,8 @@
 Надо определить максимальную выгоду вора.
 """
 
+import heapq
 from typing import List, Tuple
-
 
 # Отрезок [a, b] представлен кортежем (a, b)
 Section = Tuple[int, int]
@@ -33,11 +33,37 @@ def count_price(capacity: int, goods: List[Section]) -> float:
     return price
 
 
+def count_price_with_heap(capacity: int, goods: List[Section]) -> float:
+    """
+    Считает максимальную стоимость товаров, которые вмещаются в рюкзак.
+    Использует мин-кучу heapq из стандартной библиотеки. Сложность в худшем
+    случае O(n*log n). Если емкость рюкзака много меньше количества товаров,
+    то сложность алгоритма будет близка к линейной. Более быстрый вариант, чем
+    при использовании сортировки.
+
+    :param capacity: емкость рюкзака
+    :param goods: список данных о товарах. Каждый товар представлен в виде
+        кортежа (a, b), где a - суммарная стоимость товара, b - вес товара
+    :return: максимальная стоимость товаров
+    """
+    heap_data = [(-price / amount, amount) for price, amount in goods]
+    heapq.heapify(heap_data)
+    price = 0
+    while heap_data and capacity:
+        value, amount = heapq.heappop(heap_data)
+        can_take = min(capacity, amount)
+        price += can_take * -value
+        capacity -= can_take
+    return price
+
+
 if __name__ == '__main__':
-    assert count_price(1, []) == 0, "No goods"
-    assert count_price(0, []) == 0, "Torn backpack and no goods"
-    assert count_price(0, [(1, 2)]) == 0, "Torn backpack"
-    assert count_price(2, [(1, 1)]) == 1, "Not enough goods"
-    assert count_price(3, [(1, 1), (4, 2)]) == 5, "All goods in backpack"
-    assert count_price(1, [(1, 1), (2, 2)]) == 1, "Equal capacity goods"
-    assert count_price(1, [(1, 1), (4, 2)]) == 2, "Not enough capacity"
+
+    for func in [count_price, count_price_with_heap]:
+        assert func(1, []) == 0, "No goods"
+        assert func(0, []) == 0, "Torn backpack and no goods"
+        assert func(0, [(1, 2)]) == 0, "Torn backpack"
+        assert func(2, [(1, 1)]) == 1, "Not enough goods"
+        assert func(3, [(1, 1), (4, 2)]) == 5, "All goods in backpack"
+        assert func(1, [(1, 1), (2, 2)]) == 1, "Equal capacity goods"
+        assert func(1, [(1, 1), (4, 2)]) == 2, "Not enough capacity"
