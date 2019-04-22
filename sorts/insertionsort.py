129a0f9628773d47263a5071d7f8fbc264634cea
Anton Shelenkov <anshelen@yandex.ru>
2019-04-23 00:02:15 +0300

    feat: Add two more insertion sort algorithms

--- sorts/insertionsort.py
+++ sorts/insertionsort.py
@@ -20,14 +20,89 @@ def insertion_sort(lst: List):
     return lst
 
 
+def insertion_sort_with_buffer(lst: List):
+    """
+    Сортировка вставками с помощью буффера. Хотя буфер по-идее и не нужен,
+    почему то классический алгоритм именно с буффером.
+    """
+    for i in range(1, len(lst)):
+        buf, lst[i] = lst[i], None
+        j = i - 1
+        while j >= 0:
+            if lst[j] > buf:
+                lst[j + 1], lst[j] = lst[j], lst[j + 1]
+                j -= 1
+            else:
+                lst[j + 1] = buf
+                break
+        else:
+            lst[0] = buf
+    return lst
+
+
+def pair_insertion_sort(lst: List):
+    """
+    Парная сортировка простыми вставками. Экономит на обработке меньшего
+    элемента из пары. Для него поиск точки вставки и сама вставка осуществляются
+    только на той отсортированной части массива, в которую не входит
+    отсортированная область, задействованная для обработки большего элемента
+    из пары. Сложность алгоритма так же О(n^2), но он быстрее обычной сортировки
+    вставками. Обычно используется для сортировки малых массивов или небольших
+    участков крупных массивов.
+    """
+    for i in range(0, len(lst), 2):
+        if i == len(lst) - 1:
+            # Обычная сортировка вставками для последнего нечетного элемента
+            buf, lst[i] = lst[i], None
+            j = i - 1
+            while j >= 0:
+                if lst[j] > buf:
+                    lst[j + 1], lst[j] = lst[j], lst[j + 1]
+                    j -= 1
+                else:
+                    lst[j + 1] = buf
+                    break
+            else:
+                lst[0] = buf
+        else:
+            small, big = min(lst[i], lst[i + 1]), max(lst[i], lst[i + 1])
+            # Для дебага. Не влияет на сортировку
+            lst[i], lst[i + 1] = None, None
+            j = i - 1
+            # Проходим для большого элемента
+            while j >= 0:
+                if lst[j] > big:
+                    lst[j], lst[j + 2] = lst[j + 2], lst[j]
+                    j -= 1
+                else:
+                    lst[j + 2] = big
+                    break
+            else:
+                lst[1] = big
+
+            # Продолжаем проход теперь для малого элемента
+            while j >= 0:
+                if lst[j] > small:
+                    lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                    j -= 1
+                else:
+                    lst[j + 1] = small
+                    break
+            else:
+                lst[0] = small
+    return lst
+
+
 if __name__ == '__main__':
 
-    assert insertion_sort([]) == []
-    assert insertion_sort([1]) == [1]
-    assert insertion_sort([1, 2]) == [1, 2]
-    assert insertion_sort([2, 1]) == [1, 2]
-    assert insertion_sort([2, 1, 3]) == [1, 2, 3]
-    assert insertion_sort([2, 1, 4, 3, 3, 5]) == [1, 2, 3, 3, 4, 5]
-    assert insertion_sort([5, 3, 2, 3, 1, 4]) == [1, 2, 3, 3, 4, 5]
-    assert insertion_sort([3, 3, 2, 1, 4, 5]) == [1, 2, 3, 3, 4, 5]
-    assert insertion_sort([3, 3, 2, 1, 6, 4, 5]) == [1, 2, 3, 3, 4, 5, 6]
+    for func in [insertion_sort, insertion_sort_with_buffer,
+                 pair_insertion_sort]:
+        assert func([]) == []
+        assert func([1]) == [1]
+        assert func([1, 2]) == [1, 2]
+        assert func([2, 1]) == [1, 2]
+        assert func([2, 1, 3]) == [1, 2, 3]
+        assert func([2, 1, 4, 3, 3, 5]) == [1, 2, 3, 3, 4, 5]
+        assert func([5, 3, 2, 3, 1, 4]) == [1, 2, 3, 3, 4, 5]
+        assert func([3, 3, 2, 1, 4, 5]) == [1, 2, 3, 3, 4, 5]
+        assert func([3, 3, 2, 1, 6, 4, 5]) == [1, 2, 3, 3, 4, 5, 6]
