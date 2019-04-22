7d9dfe5f0575c6ada0c040341bc75eaf6867871f
Anton Shelenkov <anshelen@yandex.ru>
2019-04-22 03:46:02 +0300

    feat: Add algorithm of binary index search in list

--- binarysearch.py
+++ binarysearch.py
@@ -2,7 +2,7 @@
 Алгоритм двоичного поиска.
 """
 
-from typing import Sequence, TypeVar
+from typing import Sequence, TypeVar, List
 
 
 Element = TypeVar("Element")
@@ -25,6 +25,26 @@ def search(lst: Sequence[Element], el: Element) -> int:
     return -1
 
 
+def index(lst: List[Element], el: Element) -> int:
+    """
+    Возвращает такой индекс i в массиве A, что A[i-1] <= el <= A[i+1]. То есть
+    с помощью бинарного поиска находит позицию, в которую элемент мог бы быть
+    помещен, при этом не нарушив сортировки. Сложность O(log n).
+    """
+    if not lst:
+        return 0
+    left, right, m = 0, len(lst) - 1, 0
+    while left <= right:
+        m = (left + right) // 2
+        if lst[m] == el:
+            return m
+        elif lst[m] > el:
+            right = m - 1
+        else:
+            left = m + 1
+    return m if lst[m] > el else m + 1
+
+
 if __name__ == '__main__':
 
     assert search([], 1) == -1
@@ -43,3 +63,13 @@ if __name__ == '__main__':
     assert search([1, 2, 3, 4, 5, 5, 5, 5, 6, 6, 6, 7], 6) == 8
     assert search([1, 2, 3, 4, 5, 5, 5, 5, 6, 6, 6, 7], 7) == 11
     assert search([1, 2, 3, 4, 5, 5, 5, 5, 6, 6, 6, 7], 1) == 0
+
+    assert index([], 1) == 0
+    assert index([1], 1) == 0
+    assert index([2], 1) == 0
+    assert index([0], 1) == 1
+    assert index([0, 2, 3], 1) == 1
+    assert index([0, 2, 3], 2) == 1
+    assert index([0, 1, 2, 3], 1) == 1
+    assert index([0, 1, 2, 3], 4) == 4
+    assert index([0, 1, 2, 3], 0) == 0
