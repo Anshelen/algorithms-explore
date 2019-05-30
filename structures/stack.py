d37541c15d44284e1a445699feb4cca37aa614a4
Anton Shelenkov <anshelen@yandex.ru>
2019-05-30 22:14:30 +0300

    feat: Add peek method and support for len() function for a stack

--- structures/stack.py
+++ structures/stack.py
@@ -14,23 +14,34 @@ class Stack:
 
     def __init__(self) -> None:
         self._head = None
+        self._size = 0
+
+    def peek(self):
+        if self.empty():
+            raise RuntimeError('Stack is empty')
+        return self._head.value
 
     def get(self):
         if self.empty():
             raise RuntimeError('Stack is empty')
         res = self._head.value
         self._head = self._head.next
+        self._size -= 1
         return res
 
     def put(self, item):
         new_node = _Node(item, self._head if not self.empty() else None)
         self._head = new_node
+        self._size += 1
 
     def empty(self):
         return self._head is None
 
     def __bool__(self):
         return not self.empty()
+
+    def __len__(self):
+        return self._size
 
 
 class StackTest(unittest.TestCase):
@@ -45,15 +56,15 @@ class StackTest(unittest.TestCase):
         self.stack.put(1)
         self.assertFalse(self.stack.empty())
 
-    def test_put_first(self):
+    def test_push_first(self):
         self.stack.put(1)
         self.assertEqual(self.stack.get(), 1)
 
-    def test_get_empty(self):
+    def test_pop_empty(self):
         with self.assertRaises(RuntimeError):
             self.stack.get()
 
-    def test_common(self):
+    def test_push_and_pop(self):
         self.stack.put(1)
         self.stack.put(2)
         self.assertEqual(self.stack.get(), 2)
@@ -63,12 +74,25 @@ class StackTest(unittest.TestCase):
         self.stack.put(4)
         self.assertEqual(self.stack.get(), 4)
 
+    def test_peek_empty(self):
+        with self.assertRaises(RuntimeError):
+            self.stack.peek()
+
+    def test_peek_not_empty(self):
+        self.stack.put(1)
+        self.assertEqual(self.stack.peek(), 1)
+        self.assertEqual(len(self.stack), 1)
+
     def test_bool_empty(self):
         self.assertFalse(self.stack)
 
     def test_bool_not_empty(self):
         self.stack.put(0)
         self.assertTrue(self.stack)
+
+    def test_len(self):
+        self.stack.put(0)
+        self.assertEqual(len(self.stack), 1)
 
 
 if __name__ == '__main__':
